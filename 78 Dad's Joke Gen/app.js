75d442b40c26760ae2c565f72390556751fd62b6
Aman Singh <singhaman131994@gmail.com>
2023-09-15 18:59:13 +0530

    Add files via upload

---
+++ 78 Dad's Joke Gen/app.js
@@ -0,0 +1,15 @@
+// https://icanhazdadjoke.com/
+
+document.getElementById("btn").addEventListener("click", joke);
+
+async function joke() {
+  let config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  let a = await fetch("https://icanhazdadjoke.com/", config);
+  let b = await a.json();
+  document.getElementById("content").innerHTML = b.joke;
+}
