92ed19d85d0dcede398cc9ca04d6c69a6d16d30a
Aman Singh <singhaman131994@gmail.com>
2023-09-25 18:47:22 +0530

    Add files via upload

---
+++ 88 WordCounter/app.js
@@ -0,0 +1,43 @@
+const textInput = document.querySelector(".text-input");
+const wordCountElement = document.querySelector(".word-count");
+const letterCountElement = document.querySelector(".letter-count");
+const spaceCountElement = document.querySelector(".space-count");
+
+const checks = [atLeastTwoCharacters, abscenceofThreeConsecutiveCharacters];
+
+function atLeastTwoCharacters(text) {
+  const letters = text.match(/[a-z]/gi) || [];
+  return letters.length >= 2;
+}
+
+function abscenceofThreeConsecutiveCharacters(text) {
+  for (const character of text) {
+    const occurrences = Array.from(text).filter((v) => v == character).length;
+
+    if (occurrences >= 3) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+textInput.addEventListener("input", () => {
+  const splitted = textInput.value.trim().split(/[\s-]/);
+  const letterCount = (textInput.value.match(/[a-z]/gi) || []).length;
+  const spaceCount = (textInput.value.match(/\s+/g) || []).length;
+  let wordCount = 0;
+
+  outer: for (const text of splitted) {
+    for (const check of checks) {
+      if (!check(text)) {
+        continue outer;
+      }
+    }
+    wordCount++;
+  }
+
+  wordCountElement.textContent = wordCount;
+  letterCountElement.textContent = letterCount;
+  spaceCountElement.textContent = spaceCount;
+});
